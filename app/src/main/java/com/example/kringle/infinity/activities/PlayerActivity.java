31e701655136e14248dd3bfbdd5defcb50eec3f9
Abror <abror@ubuntu.ubuntu-domain>
2018-09-20 20:17:29 +0500

    added mp3 player

--- app/src/main/java/com/example/kringle/infinity/activities/PlayerActivity.java
+++ app/src/main/java/com/example/kringle/infinity/activities/PlayerActivity.java
@@ -2,6 +2,8 @@ package com.example.kringle.infinity.activities;
 
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,23 +13,51 @@ import android.widget.Toast;
 import com.example.kringle.infinity.R;
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class PlayerActivity extends AppCompatActivity {
+public class PlayerActivity extends AppCompatActivity implements ExoPlayer.EventListener {
 
     private boolean isPlaying = false;
 
     @BindView(R.id.play_pause_btn)
     ImageView play_pause_btn;
-    AdaptiveTrackSelection adaptive;
 
-    private MediaPlayer mediaPlayer;
+    private Handler mainHandler;
+    private RenderersFactory renderersFactory;
+    private BandwidthMeter bandwidthMeter;
+    private LoadControl loadControl;
+    private DataSource.Factory dataSourceFactory;
+    private ExtractorsFactory extractorsFactory;
+    private MediaSource mediaSource;
+    private TrackSelection.Factory trackSelectionFactory;
+    private SimpleExoPlayer player;
+    private String streamUrl; //bbc world service url
+    private TrackSelector trackSelector;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,23 +65,97 @@ public class PlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_player);
         ButterKnife.bind(this);
 
-        String url_link = getIntent().getStringExtra("link");
-        Uri uri = Uri.parse(url_link);
+        streamUrl = getIntent().getStringExtra("link");
+        MainActivity.prefConfig.displayToast(streamUrl);
 
-        mediaPlayer = MediaPlayer.create(this, uri);
+        renderersFactory = new DefaultRenderersFactory(getApplicationContext());
+        bandwidthMeter = new DefaultBandwidthMeter();
+        trackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        trackSelector = new DefaultTrackSelector(trackSelectionFactory);
+        loadControl = new DefaultLoadControl();
+
+        player = ExoPlayerFactory.newSimpleInstance(renderersFactory, trackSelector, loadControl);
+        player.addListener(this);
+
+
+        dataSourceFactory = new DefaultDataSourceFactory(getApplicationContext(), "ExoplayerDemo");
+        extractorsFactory = new DefaultExtractorsFactory();
+        mainHandler = new Handler();
+        mediaSource = new ExtractorMediaSource(Uri.parse(streamUrl),
+                dataSourceFactory,
+                extractorsFactory,
+                mainHandler,
+                null);
+
+        player.prepare(mediaSource);
+
 
         play_pause_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!isPlaying) {
-                    mediaPlayer.start();
+                    player.setPlayWhenReady(true);
+                    Toast.makeText(PlayerActivity.this, "Exoplayer is playing.", Toast.LENGTH_SHORT).show();
+                    play_pause_btn.setBackgroundResource(R.drawable.ic_pause);
+                    isPlaying = true;
                 } else {
-                    mediaPlayer.pause();
-                    play_pause_btn.set
+                    player.setPlayWhenReady(false);
+                    Toast.makeText(PlayerActivity.this, "Exoplayer is on pause.", Toast.LENGTH_SHORT).show();
+                    play_pause_btn.setBackgroundResource(R.drawable.ic_play);
+                    isPlaying = true;
                 }
-
             }
         });
+
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, @Nullable Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
 
     }
 }
